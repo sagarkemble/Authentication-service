139794b16e8b65a14588fe545d96a16579ddad23
Sagar Kemble <sagarkemble.sasa@gmail.com>
2026-04-04 19:15:02 +0530

    feat: add validate in auth middleware

---
+++ src/modules/auth/auth.middleware.ts
@@ -0,0 +1,32 @@
+import type { Request, Response, NextFunction } from "express";
+import ApiError from "../../common/utils/api-error.js";
+import { verifyAccessToken } from "../../common/utils/jwt.utils.js";
+import User from "./auth.model.js";
+
+const validate = async function (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  let token;
+
+  if (req.headers?.authorization?.startsWith("Bearer"))
+    token = req.headers.authorization.split(" ")[1];
+
+  if (!token) throw ApiError.unauthorized("Not authenticated");
+
+  const decoded = verifyAccessToken(token);
+  // @ts-ignore
+  const user = await User.findById(decoded.id);
+
+  if (!user) throw ApiError.unauthorized("User no longer exists");
+
+  // @ts-ignore
+  req.user = {
+    id: user._id,
+    role: user.role,
+    name: user.name,
+    email: user.email,
+  };
+  next();
+};
