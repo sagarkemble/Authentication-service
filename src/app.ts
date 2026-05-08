762b24557573a4a2b2d1be9d6c14469754f36ff4
Sagar Kemble <sagarkemble.sasa@gmail.com>
2026-05-08 11:17:04 +0530

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import { authRouter } from "./modules/auth/auth.routes";
 import globalErrorHandler from "./common/middleware/globlaErrorHandler.middleware";
 import { userRouter } from "./modules/user/user.routes";
@@ -7,6 +8,12 @@ import notFoundHandler from "./common/middleware/notFound.middleware";
 import ApiResponse from "./common/utils/api-response.utils";
 
 export const app = express();
+app.use(
+  cors({
+    origin: process.env.ALLOWED_ORIGINS?.split(",") || [],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
